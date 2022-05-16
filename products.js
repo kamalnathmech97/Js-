dfcd08ad5c99a2f5201d990b3dd2842e2a9d6cee
kamalnathmech97 <97251534+kamalnathmech97@users.noreply.github.com>
2022-05-16 21:18:45 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 1000,
+    instock: 100,
+    description:
+      "Levi's Mens Henley Neck Printed T-Shirt",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 1999,
+    instock: 43,
+    description:
+      "Levi's Mens V Neck T-Shirt",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 999,
+    instock: 10,
+    description:
+      "Levi's Mens Round Neck Printed T-Shirt",
+    imgSrc: "./img/t3.png",
+  },
+  {
+    id: 3,
+    name: "T-shirt 4",
+    price: 777,
+    instock: 5,
+    description:
+      "Levi's Mens Round Neck Printed T-Shirt",
+    imgSrc: "./img/t4.png",
+  },
+  {
+    id: 4,
+    name: "T-shirt 5",
+    price: 660,
+    instock: 4,
+    description:
+      "Levi's Mens Round Neck Printed T-Shirt",
+    imgSrc: "./img/t5.png",
+  },
+  {
+    id: 5,
+    name: "T-shirt 6",
+    price: 880,
+    instock: 40,
+    description:
+      "Levi's Mens Round Neck Printed T-Shirt",
+    imgSrc: "./img/t6.png",
+  },
+];
